6d042ce39001cd068591514eecd7948429c5df4a
ElDav1d <dvivoes@yahoo.es>
2023-03-03 19:38:32 +0100

    Resolves Iteration 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import NotFound from "./pages/NotFound";
+import ListBeers from "./pages/ListBeers";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/beers" element={<ListBeers />} />
         <Route path="/error" element={<Error />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/ListBeers.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+function ListBeers() {
+  return (
+    <>
+      <Header />
+      <h1>ListBeers</h1>
+    </>
+  );
+}
+
+export default ListBeers;
