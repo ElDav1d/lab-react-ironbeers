96b953d924aa170b0cf91d71c5419d4fa447ea96
ElDav1d <dvivoes@yahoo.es>
2023-03-03 19:19:20 +0100

    Resolves Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <h1>YAY</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
