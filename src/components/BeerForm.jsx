0c0a2ae78afb37df69efdf4580ec899c97a88c8b
ElDav1d <dvivoes@yahoo.es>
2023-03-05 13:24:56 +0100

    style form

--- src/components/BeerForm.jsx
+++ src/components/BeerForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 import LoadSpinner from "../components/LoadSpinner/LoadSpinner";
 
 function BeerForm({ setNewBeer, isSending }) {
@@ -31,72 +33,86 @@ function BeerForm({ setNewBeer, isSending }) {
   };
 
   return (
-    <form>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        onChange={handleInput}
-        value={beerInput.name}
-      />
-      <br />
-      <br />
-      <label htmlFor="tagline">Tagline</label>
-      <input
-        type="text"
-        name="tagline"
-        onChange={handleInput}
-        value={beerInput.tagline}
-      />
-      <br />
-      <br />
-      <label htmlFor="descriptiom">Description</label>
-      <textarea
-        type="text"
-        name="description"
-        rows="10"
-        cols="50"
-        onChange={handleInput}
-        value={beerInput.description}
-      ></textarea>
-      <br />
-      <br />
-      <label htmlFor="first_brewed">First Brewed</label>
-      <input
-        type="text"
-        name="first_brewed"
-        onChange={handleInput}
-        value={beerInput.first_brewed}
-      />
-      <br />
-      <br />
-      <label htmlFor="first_brewed">Brewers Tips</label>
-      <input
-        type="text"
-        name="brewers_tips"
-        onChange={handleInput}
-        value={beerInput.brewers_tips}
-      />
-      <br />
-      <br />
-      <label htmlFor="attenuation_level">Attenuation Level</label>
-      <input
-        type="number"
-        name="attenuation_level"
-        onChange={handleInput}
-        value={beerInput.attenuation_level}
-      />
-      <br />
-      <br />
-      <label htmlFor="contributed_by">Contributed By</label>
-      <input
-        type="text"
-        name="contributed_by"
-        onChange={handleInput}
-        value={beerInput.contributed_by}
-      />
-      <br />
-      <br />
+    <Container as="form">
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="name">Name</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          name="name"
+          onChange={handleInput}
+          value={beerInput.name}
+          disabled={isSending}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="tagline">Tagline</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          name="tagline"
+          onChange={handleInput}
+          value={beerInput.tagline}
+          disabled={isSending}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="descriptiom">Description</Form.Label>
+        <Form.Control
+          size="lg"
+          as="textarea"
+          name="description"
+          rows="10"
+          cols="50"
+          onChange={handleInput}
+          value={beerInput.description}
+          disabled={isSending}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="first_brewed">First Brewed</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          name="first_brewed"
+          onChange={handleInput}
+          value={beerInput.first_brewed}
+          disabled={isSending}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="first_brewed">Brewers Tips</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          name="brewers_tips"
+          onChange={handleInput}
+          value={beerInput.brewers_tips}
+          disabled={isSending}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="attenuation_level">Attenuation Level</Form.Label>
+        <Form.Control
+          size="lg"
+          type="number"
+          name="attenuation_level"
+          onChange={handleInput}
+          value={beerInput.attenuation_level}
+          disabled={isSending}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="contributed_by">Contributed By</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          name="contributed_by"
+          onChange={handleInput}
+          value={beerInput.contributed_by}
+          disabled={isSending}
+        />
+      </Form.Group>
 
       {isSending ? (
         <LoadSpinner hidddenText="Sending new beer" />
@@ -105,7 +121,7 @@ function BeerForm({ setNewBeer, isSending }) {
           ADD NEW
         </Button>
       )}
-    </form>
+    </Container>
   );
 }
 
